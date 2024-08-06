c2e1605e37fab425efd2159dfd4ea3a5df6928c2
Sam Foucart <samfoucart25@gmail.com>
2024-08-05 21:29:50 -0400

    added prettier

--- frontend/portfolio-2024/tailwind.config.js
+++ frontend/portfolio-2024/tailwind.config.js
@@ -1,23 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: ['Epilogue', 'sans-serif'],
-      serif: ['Lora', 'serif'],
+      sans: ["Epilogue", "sans-serif"],
+      serif: ["Lora", "serif"],
     },
     extend: {
       colors: {
-        'bully-bg': '#ffd567',
-        'bully-bg-d': '#fbd062',
+        "bully-bg": "#ffd567",
+        "bully-bg-d": "#fbd062",
       },
       width: {
-        '6xl': '72rem',
-      }
+        "6xl": "72rem",
+      },
     },
   },
   plugins: [],
-}
+};
